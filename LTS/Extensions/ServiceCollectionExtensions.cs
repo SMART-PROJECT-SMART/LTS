2b665b31e88b3792cf65f634c094839d74d64a6f
kalush666 <jonathan.kalush@gmail.com>
2025-10-23 17:24:27 +0300

    removed redunent service

--- LTS/Extensions/ServiceCollectionExtensions.cs
+++ LTS/Extensions/ServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@
     {
         public static IServiceCollection AddUAVWantedFieldsManager(this IServiceCollection services)
         {
-            services.AddSingleton<Services.UAVWantedFieldsManager.IUAVWantedFieldsManager, Services.UAVWantedFieldsManager.UAVWantedFieldsManager>();
+            services.AddTransient<Services.UAVWantedFieldsManager.IUAVWantedFieldsManager, Services.UAVWantedFieldsManager.UAVWantedFieldsManager>();
             return services;
         }
     }
--- LTS/Models/ClientSubscription.cs
+++ LTS/Models/ClientSubscription.cs
@@ -1,8 +1,12 @@
-﻿namespace LTS.Models
+﻿using Core.Common.Enums;
+
+namespace LTS.Models
 {
     public struct ClientSubscription
     {
-        public string ConnectionId { get; set; }
+        public string? ConnectionId { get; set; }
         public string SessionId { get; set; }
+        public DateTime LastActive { get; set; }
+        public Dictionary<int, HashSet<TelemetryFields>> WantedUAVsFields;
     }
 }
