db281e438f22690013d8580484bc07824db6962b
kalush666 <jonathan.kalush@gmail.com>
2025-10-23 14:59:15 +0300

    basic config

--- LTS/obj/Debug/net9.0/LTS.AssemblyInfo.cs
+++ LTS/obj/Debug/net9.0/LTS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LTS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d70dc635143bb6261b7bf1ff68d7fc0c5f2d41a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LTS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LTS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
