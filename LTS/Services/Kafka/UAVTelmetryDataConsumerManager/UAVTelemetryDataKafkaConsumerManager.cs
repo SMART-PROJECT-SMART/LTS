c621bc42f2c3f49863c2a8f90e7cfa6968b57e29
kalush666 <jonathan.kalush@gmail.com>
2025-12-23 19:32:09 +0200

    [refactor] thread safty

--- LTS/Services/Kafka/UAVTelmetryDataConsumerManager/UAVTelemetryDataKafkaConsumerManager.cs
+++ LTS/Services/Kafka/UAVTelmetryDataConsumerManager/UAVTelemetryDataKafkaConsumerManager.cs
@@ -1,4 +1,5 @@
-﻿using Confluent.Kafka;
+﻿using System.Collections.Concurrent;
+using Confluent.Kafka;
 using LTS.Services.Kafka.UAVTelemetryDataConsumer.Interfaces;
 using LTS.Services.Kafka.UAVTelmetryDataConsumerManager.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,36 +8,34 @@ namespace LTS.Services.Kafka.UAVTelmetryDataConsumerManager
 {
     public class UAVTelemetryDataKafkaConsumerManager : IUAVTelemetryDataKafkaConsumerManager
     {
-        private readonly Dictionary<string, IUAVTelemetryDataKafkaConsumer> _consumers;
+        private readonly ConcurrentDictionary<string, IUAVTelemetryDataKafkaConsumer> _consumers;
         private readonly IServiceProvider _serviceProvider;
 
         public UAVTelemetryDataKafkaConsumerManager(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
-            _consumers = new Dictionary<string, IUAVTelemetryDataKafkaConsumer>();
+            _consumers = new ConcurrentDictionary<string, IUAVTelemetryDataKafkaConsumer>();
         }
 
         public void AddConsumer(string tailId)
         {
-            if (_consumers.ContainsKey(tailId))
+            _consumers.GetOrAdd(tailId, key =>
             {
-                return;
-            }
-
-            IUAVTelemetryDataKafkaConsumer? newConsumer =
-                _serviceProvider.GetService<IUAVTelemetryDataKafkaConsumer>();
-            if (newConsumer == null)
-                return;
-            newConsumer.SubsribeToTopic(tailId);
-            _consumers.Add(tailId, newConsumer);
+                IUAVTelemetryDataKafkaConsumer? newConsumer =
+                    _serviceProvider.GetService<IUAVTelemetryDataKafkaConsumer>();
+                if (newConsumer == null)
+                    throw new InvalidOperationException("Failed to create UAV telemetry data consumer");
+                newConsumer.SubsribeToTopic(key);
+                return newConsumer;
+            });
         }
 
         public void RemoveConsumer(string tailId)
         {
-            if (!_consumers.TryGetValue(tailId, out var consumerToRemove))
-                return;
-            consumerToRemove.Dispose();
-            _consumers.Remove(tailId);
+            if (_consumers.TryRemove(tailId, out IUAVTelemetryDataKafkaConsumer? consumerToRemove))
+            {
+                consumerToRemove.Dispose();
+            }
         }
 
         public IEnumerable<ConsumeResult<string, byte[]>> ConsumeUAVTelemetryData()
